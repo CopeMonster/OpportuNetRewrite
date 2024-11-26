c5d4c5ab8e74675b25311dd59d81f1fd8555c577
copemonster <alankassymbek@gmail.com>
2024-11-26 21:39:31 +0500

    README.MD, description of endpoints

---
+++ src/main/java/com/windowsxp/opportunetrewrite/controllers/VacancyController.java
@@ -0,0 +1,12 @@
+package com.windowsxp.opportunetrewrite.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/vacancies")
+@RequiredArgsConstructor
+public class VacancyController {
+
+}
