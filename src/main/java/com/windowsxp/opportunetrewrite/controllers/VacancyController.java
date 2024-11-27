ee9063aeeee2bacbfdd28fa0954653fae151a274
copemonster <alankassymbek@gmail.com>
2024-11-27 16:57:22 +0500

    VacancyController getAllVacancies was added

--- src/main/java/com/windowsxp/opportunetrewrite/controllers/VacancyController.java
+++ src/main/java/com/windowsxp/opportunetrewrite/controllers/VacancyController.java
@@ -4,11 +4,14 @@ import com.windowsxp.opportunetrewrite.assemblers.VacancyAssembler;
 import com.windowsxp.opportunetrewrite.entities.Vacancy;
 import com.windowsxp.opportunetrewrite.services.VacancyService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/vacancies")
@@ -22,5 +25,15 @@ public class VacancyController {
         Vacancy vacancy = vacancyService.getVacancyById(vacancyId);
 
         return vacancyAssembler.toModel(vacancy);
+    }
+
+    @GetMapping
+    public CollectionModel<EntityModel<Vacancy>> getAllVacancies() {
+        List<EntityModel<Vacancy>> vacancies = vacancyService.getVacancies()
+                .stream()
+                .map(vacancyAssembler::toModel)
+                .toList();
+
+        return CollectionModel.of(vacancies);
     }
 }
